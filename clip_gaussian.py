c6c0b461e8dc490060248d057b30fe0572cf36d0
LadyZizi34 <laiseaquino34@gmail.com>
2018-06-15 13:29:31 -0300

    added gaussian and sequence viewers

---
+++ clip_gaussian.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+files = [f for f in os.listdir('Images/tif')]
+plt.ion()
+
+plt.figure(figsize=(4, 3))
+img = plt.imshow(plt.imread('Images/Filtered/Gaussian/085.png'))
+plt.axis('off')
+while(1):
+	for f in files:
+		im = plt.imread('Images/Filtered/Gaussian/' + f[:3] + '.png')
+		img.set_data(im)
+		plt.pause(.02)
+		plt.draw()
